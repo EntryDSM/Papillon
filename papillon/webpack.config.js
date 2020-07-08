e48791376e2059f1641d0f59c4667e13d3fe8bc8
so_ohi <jsh167528@gmail.com>
2020-07-08 15:58:55 +0900

    [Add] setting file
    add favicon
    move & add setting file

--- papillon/webpack.config.js
+++ papillon/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ["*", ".js", ".ts", ".tsx"],
+    extensions: ['*', '.js', '.ts', '.tsx'],
     alias: {
-      assets: path.resolve(__dirname, "src/assets"),
-      api: path.resolve(__dirname, "src/data/api"),
-      modules: path.resolve(__dirname, "src/data/modules"),
-      components: path.resolve(__dirname, "src/components"),
-      hooks: path.resolve(__dirname, "src/hooks"),
-      page: path.resolve(__dirname, "src/page"),
-      styles: path.resolve(__dirname, "src/styles")
-    }
+      assets: path.resolve(__dirname, 'src/assets'),
+      api: path.resolve(__dirname, 'src/data/api'),
+      modules: path.resolve(__dirname, 'src/data/modules'),
+      components: path.resolve(__dirname, 'src/components'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      page: path.resolve(__dirname, 'src/page'),
+      styles: path.resolve(__dirname, 'src/styles'),
+    },
   },
   module: {
     rules: [
@@ -25,32 +25,32 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "ts-loader"
-        }
+          loader: 'ts-loader',
+        },
       },
       {
         test: /\.png$/,
         use: {
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
             limit: 100000,
-            name: "[name].[ext]?[hash]",
-            publicPath: "/dist"
-          }
-        }
-      }
-    ]
+            name: '[name].[ext]?[hash]',
+            publicPath: '/dist',
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
-  ]
+      template: './public/index.html',
+    }),
+  ],
 };
