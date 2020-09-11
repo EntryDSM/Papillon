e73b7f20772aadce41937dfe96d08e42ee8f6609
so_ohi <jsh167528@gmail.com>
2020-09-11 17:16:59 +0900

    ADD: routing

--- papillon/src/App.tsx
+++ papillon/src/App.tsx
@@ -2,16 +2,22 @@ import * as React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 import Login from 'page/Login';
+import Statistics from 'page/Statistics';
+import Applicant from 'page/Applicant';
+import Admission from 'page/Admission';
+import CheckToken from './components/common/CheckToken';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/login" component={() => <Login />} />
-        <Route exact path="/" component={() => <></>} />
-        <Route exact path="/applicant" component={() => <></>} />
-        <Route exact path="/admission" component={() => <></>} />
-        <Route component={() => <Redirect to="/" />} />
+        <CheckToken>
+          <Route exact path="/" component={() => <Statistics />} />
+          <Route exact path="/applicant" component={() => <Applicant />} />
+          <Route exact path="/admission" component={() => <Admission />} />
+          <Route component={() => <Redirect to="/" />} />
+        </CheckToken>
       </Switch>
     </BrowserRouter>
   );
