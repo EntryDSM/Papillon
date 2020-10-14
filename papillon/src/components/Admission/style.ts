0b0be32a58b06f12683314afc0a374e05d52522a
so_ohi <jsh167528@gmail.com>
2020-10-14 19:35:46 +0900

    Modify: style error

--- papillon/src/components/Admission/style.ts
+++ papillon/src/components/Admission/style.ts
@@ -5,25 +5,33 @@ import { darkPrimaryColor, primaryTextColor } from '../../styles/colors';
 export const ContentContainer = styled.div`
   width: 70%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   background: white;
   box-sizing: border-box;
-  padding-top: 7%;
 
-  > h1 {
+  div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
     font-size: 58px;
     color: ${primaryTextColor};
     margin-top: 2%;
-    margin-bottom: 4%;
+    margin-bottom: 2%;
     letter-spacing: 2.56px;
   }
 
-  > p {
+  p {
     font-size: 20px;
     color: ${darkPrimaryColor};
     letter-spacing: 0.72px;
+
+    &:first-child {
+      margin-top: 8%;
+    }
   }
 `;
 
